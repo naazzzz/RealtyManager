a2f63d294d6fe3889c967f4cc5da7b5c1312a84c
artem <artem_berezhnov@mail.ru>
2022-11-15 02:05:34 +0300

    Добавлен post-запрос банк-игрок

--- src/main/java/com/vpr/monopoly/realtymanager/controller/RealtyController.java
+++ src/main/java/com/vpr/monopoly/realtymanager/controller/RealtyController.java
@@ -1,18 +1,20 @@
 package com.vpr.monopoly.realtymanager.controller;
 
+import com.vpr.monopoly.realtymanager.model.ActionDto;
+import com.vpr.monopoly.realtymanager.model.PlayerDto;
 import com.vpr.monopoly.realtymanager.model.RealtyCardDto;
-import com.vpr.monopoly.realtymanager.service.RealtyCardClient;
+import com.vpr.monopoly.realtymanager.service.RealtyCardService;
 import com.vpr.monopoly.realtymanager.service.RealtyManagerService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
+import java.util.Map;
 
 @Tag(name = "REALTY CONTROLLER", description = "API для управления медежером недвижимости")
 @RestController
@@ -24,12 +26,18 @@ public class RealtyController {
 
     private final RealtyManagerService realtyservice;
 
-    private final RealtyCardClient rc;
 
     @GetMapping("/allcards")
-    public ResponseEntity<List<RealtyCardDto>> endGame(){
-        List<RealtyCardDto> all_cards = realtyservice.getAllRealtyCards(rc);
+    public ResponseEntity<List<RealtyCardDto>> allCard(){
+        List<RealtyCardDto> all_cards = realtyservice.getAllRealtyCards();
         log.info("get all cards");
         return ResponseEntity.ok(all_cards);
     }
+    @PostMapping("/tobank")
+    public ResponseEntity<ActionDto> toBank(@RequestBody ActionDto actionDto)
+    {
+        ActionDto action =realtyservice.playerToBankInteraction(actionDto);
+        return ResponseEntity.ok(action);
+    }
+
 }
