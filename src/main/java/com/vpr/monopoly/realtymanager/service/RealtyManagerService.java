d903dd436e9ec7397e97e0c678c4663ed5282d89
artem <artem_berezhnov@mail.ru>
2022-11-19 02:36:35 +0300

    добавил в интерфейс метод для проверки на монополию недвижимости

--- src/main/java/com/vpr/monopoly/realtymanager/service/RealtyManagerService.java
+++ src/main/java/com/vpr/monopoly/realtymanager/service/RealtyManagerService.java
@@ -1,12 +1,14 @@
 package com.vpr.monopoly.realtymanager.service;
 
 import com.vpr.monopoly.realtymanager.model.ActionDto;
+import com.vpr.monopoly.realtymanager.model.PlayerDto;
 import com.vpr.monopoly.realtymanager.model.RealtyCardDto;
 
 
 import java.util.List;
 
 public interface RealtyManagerService {
+    PlayerDto testColorMonopoly(PlayerDto player);
     ActionDto playerToBankInteraction(ActionDto action);
 
     boolean isPlayerToBankInteraction(ActionDto action);
